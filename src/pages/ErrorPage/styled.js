6604146987c9955ab63d8c41e878812394e27bd3
Talita Miguel <talita.leonardo@gmail.com>
2022-09-04 20:27:37 -0300

    error page

--- src/pages/ErrorPage/styled.js
+++ src/pages/ErrorPage/styled.js
@@ -17,4 +17,9 @@ export const ErrorImage = styled.img`
 export const ErrorText = styled.h1`
   font-size: 4rem;
   color: #b7312c;
+
+  @media (max-width: 450px) {
+    text-align: center;
+    font-size: 3.5rem;
+  }
 `;
